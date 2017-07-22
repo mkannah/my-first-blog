2df895523626035e0de315ce6def85f231f45cea
mmkannah <mmkannah@gmail.com>
2017-07-22 22:56:45 +0800

    fixed logout url

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/login/$', auth_views.login, name='login'),
-    url(r'^/accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
